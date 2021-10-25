1c8f33dc8497562f1dfa57f8970d0c4a75ccd4ff
zack-mwangi <zackmwangi998@gmail.com>
2021-10-25 15:31:56 +0300

    UPDATE ON THE NOTES

--- app/src/main/java/com/example/architectures/AddEditNoteActivity.java
+++ app/src/main/java/com/example/architectures/AddEditNoteActivity.java
@@ -11,12 +11,14 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
-public class AddNoteActivity extends AppCompatActivity {
+public class AddEditNoteActivity extends AppCompatActivity {
 
     private EditText editTextTitle;
     private EditText editTextDescription;
     private NumberPicker numberPickerPriority;
 
+    public static final String EXTRA_ID =
+            "com.example.architectures.EXTRA_ID";
     public static final String EXTRA_TITLE =
             "com.example.architectures.EXTRA_TITLE";
     public static final String EXTRA_DESCRIPTION =
@@ -35,7 +37,17 @@ public class AddNoteActivity extends AppCompatActivity {
         numberPickerPriority.setMinValue(1);
         numberPickerPriority.setMaxValue(10);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
-        setTitle("Add Note");
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra(EXTRA_ID)) {
+            setTitle("Edit Note");
+            editTextTitle.setText(intent.getStringExtra(EXTRA_TITLE));
+            editTextDescription.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+            numberPickerPriority.setValue(intent.getIntExtra(EXTRA_PRIORITY, 1));
+        } else {
+            setTitle("Add Note");
+        }
     }
 
     @Override
@@ -67,6 +79,12 @@ public class AddNoteActivity extends AppCompatActivity {
         data.putExtra(EXTRA_TITLE, title);
         data.putExtra(EXTRA_DESCRIPTION, description);
         data.putExtra(EXTRA_PRIORITY, priority);
+
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if (id != -1) {
+            data.putExtra(EXTRA_ID, id); //FOR UPDATES
+        }
+
         setResult(RESULT_OK, data);
         finish();
     }
