bcf6fd53df6242296580afa31c8dfd526719ef16
zac-mwangi <zackmwangi998@gmail.com>
2022-01-15 09:24:02 +0300

    update On DiffUtil {Animations on the recycler View}

--- app/src/main/java/com/example/architectures/NoteDatabase.java
+++ app/src/main/java/com/example/architectures/NoteDatabase.java
@@ -19,9 +19,8 @@ public abstract class NoteDatabase extends RoomDatabase {
     public abstract NoteDao noteDao(); //use method to access our Dau //room subclasses
     public static synchronized NoteDatabase getInstance(Context context){ //singleton, only one thread can access the instance
         if (instance == null){
-            //instance = Room.databaseBuilder(context.getApplicationContext(),NoteDatabase.class,"note_database").fallbackToDestructiveMigration().build();
-            instance = Room.databaseBuilder(context.getApplicationContext(),NoteDatabase.class,"note_database").fallbackToDestructiveMigration().addCallback(roomCallback).build();
-
+            instance = Room.databaseBuilder(context.getApplicationContext(),NoteDatabase.class,"note_database").fallbackToDestructiveMigration().build();
+            //instance = Room.databaseBuilder(context.getApplicationContext(),NoteDatabase.class,"note_database").fallbackToDestructiveMigration().addCallback(roomCallback).build();
         }
         return instance;
         //next repository class, an abstraction layer between data source and rest of the app eg SQLITE AND SERVERS,provides clean API to rest of the app, the view model calls API from it
@@ -47,20 +46,20 @@ public abstract class NoteDatabase extends RoomDatabase {
             noteDao.insert(new Note("Title1","Description1",1));
             noteDao.insert(new Note("Title2","Description2",2));
             noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title2","Description2",2));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title2","Description2",2));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title2","Description2",2));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title2","Description2",2));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title2","Description2",2));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title3","Description3",3));
-            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title2","Description2",2));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title2","Description2",2));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title2","Description2",2));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title2","Description2",2));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title2","Description2",2));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title3","Description3",3));
+//            noteDao.insert(new Note("Title3","Description3",3));
             return null;
         }
     }
